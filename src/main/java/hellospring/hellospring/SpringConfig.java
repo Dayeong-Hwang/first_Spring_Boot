bf4a3eafd9b6f542e284db0b7b50b3f003087933
hdy <d-yeong@naver.com>
2023-01-15 21:00:06 +0900

    JDBC 연결

--- src/main/java/hellospring/hellospring/SpringConfig.java
+++ src/main/java/hellospring/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hellospring.hellospring;
 
+import hellospring.hellospring.repository.JdbcMemberRepository;
 import hellospring.hellospring.repository.MemberRepository;
 import hellospring.hellospring.repository.MemoryMemberRepository;
 import hellospring.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private  DataSource dataSource;
+    @Autowired
+    public  SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
 
@@ -16,6 +26,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository(){
 
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
